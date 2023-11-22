30f96a65920de04b2a282641c2b179b410aed167
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-11-22 17:01:09 +0700

    fixing home component

--- monolit/src/Components/Home.js
+++ monolit/src/Components/Home.js
@@ -4,16 +4,17 @@ import { Link } from 'react-router-dom';
 import Loader from './Loader';
 
 const Home = ({ loading, setLoading }) => {
-    const [ products , setProducts ] = useState([]);
+    const [products, setProducts] = useState([]);
     useEffect(() => {
-        if(!products.length){
+        if (!products.length) {
             fetechProducts()
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [products])
 
-   async  function fetechProducts () {
+    async function fetechProducts() {
         setLoading(true)
-        const { data } = await  axios.get("https://fakestoreapi.com/products?limit=3")
+        const { data } = await axios.get("https://fakestoreapi.com/products?limit=3")
         setProducts(data);
         setLoading(false)
     }
@@ -25,36 +26,79 @@ const Home = ({ loading, setLoading }) => {
             <div className="flex flex-col lg:flex-row justify-center items-center space-y-6 lg:space-y-0">
                 <div className="w-80 sm:w-auto flex flex-col justify-start items-start">
                     <div>
-                        <p className="text-3xl xl:text-4xl font-semibold leading-9 text-gray-800 dark:text-white">International Ecommerce, We Are Here</p>
+                        <p className="text-3xl xl:text-4xl font-semibold leading-9 text-gray-800 dark:text-black">
+                            International Ecommerce, We Are Here
+                        </p>
                     </div>
                     <div className="mt-4 lg:w-4/5 xl:w-3/5">
-                        <p className="text-base leading-6 text-gray-600 dark:text-white">Same tech for less
-                            Save by shopping on Shop.</p>
+                        <p className="text-base leading-6 text-gray-600 dark:text-black">
+                            Same tech for less Save by shopping on Shop.
+                        </p>
                     </div>
                     <div className="mt-16 w-full">
-                        <Link to="/products">
-                            <button className="px-4 bg-green-400 rounded-md dark:bg-white dark:text-gray-900 dark:hover:scale-105  flex justify-between items-center w-full lg:w-72 h-14 text-white focus:ring-2 outline-none  focus:ring-offset-2  dark:hover:bg-gray-100">
-                                <p className="text-xl font-medium leading-5 bg-green-400 py-3 px-5 text-white rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">See More</p>
-                                <svg className="dark:text-gray-900" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M6.66663 16H25.3333" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M20 21.3333L25.3333 16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M20 10.6667L25.3333 16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
-                            </button>
-
-                        </Link>
+                    <Link to="/products">
+              <button className="px-4 bg-green-400 rounded-md dark:bg-white dark:text-gray-900 dark:hover:scale-105  flex justify-between items-center w-full lg:w-72 h-14 text-white focus:ring-2 outline-none  focus:ring-offset-2  dark:hover:bg-gray-100">
+                <p className="text-xl font-medium leading-5 bg-green-400 py-3 px-5 text-white rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
+                  See More
+                </p>
+                <svg
+                  className="dark:text-gray-900"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M6.66663 16H25.3333"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M20 21.3333L25.3333 16"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M20 10.6667L25.3333 16"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+            </Link>
                     </div>
                 </div>
 
                 <div className="flex flex-col sm:flex-row jusitfy-center items-center sm:space-x-5 xl:space-x-8 space-y-4 sm:space-y-0">
                     <div className="">
-                        <img className="hidden lg:block" src="https://m.media-amazon.com/images/I/71x7+zZCNBL._SL1500_.jpg" alt="sofa" />
-                        <img className="w-80 sm:w-auto lg:hidden" src="https://m.media-amazon.com/images/I/71x7+zZCNBL._SL1500_.jpg" alt="sofa" />
+                        <img
+                            className="hidden lg:block"
+                            src="https://m.media-amazon.com/images/I/71x7+zZCNBL._SL1500_.jpg"
+                            alt="sofa"
+                        />
                     </div>
                     <div className="flex flex-col justify-center items-center space-y-4 sm:space-y-0 sm:space-y-5 lg:space-y-5 xl:space-y-8">
                         <div>
-                            <img className="hidden lg:block" src="https://media.istockphoto.com/id/860853894/photo/red-headphones-isolated-on-a-white-background.jpg?s=612x612&w=0&k=20&c=qNtAs69Yalkq__YfZp2gu8w2mEcw0ssTnY6L-4mFWRI=" alt="chairs" />
-                            <img className="w-80 sm:w-auto lg:hidden" src="https://media.istockphoto.com/id/860853894/photo/red-headphones-isolated-on-a-white-background.jpg?s=612x612&w=0&k=20&c=qNtAs69Yalkq__YfZp2gu8w2mEcw0ssTnY6L-4mFWRI=" alt="chairs" />
+                            <img
+                                className="hidden lg:block"
+                                src="https://media.istockphoto.com/id/860853894/photo/red-headphones-isolated-on-a-white-background.jpg?s=612x612&w=0&k=20&c=qNtAs69Yalkq__YfZp2gu8w2mEcw0ssTnY6L-4mFWRI="
+                                alt="headphone"
+                            />
+                        </div>
+                        <div>
+                            <img
+                                className="hidden lg:block"
+                                src="https://dlcdnwebimgs.asus.com/gain/5183d61e-e9fe-4734-8270-d937a0cafc8d/"
+                                alt="laptop"
+                                style={{ maxWidth: '100%', maxHeight: '400px' }}
+                            />
                         </div>
                     </div>
                 </div>
