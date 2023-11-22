bb0fc9fc8989ee0aaf0b9aa20e0edaa3dc5acfbf
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-11-22 15:49:12 +0700

    refactor monilith component

--- monolit/src/Components/Loader.jsx
+++ monolit/src/Components/Loader.jsx
@@ -1,18 +1,18 @@
-export default () => {
+const Loader = () => {
   return (
-    <div class="flex items-center justify-center min-w-full h-screen border-4">
-      <div class="flex justify-center items-center space-x-1 text-sm text-gray-700">
+    <div className="flex items-center justify-center min-w-full h-screen border-4">
+      <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
         <svg
           fill="none"
-          class="w-6 h-6 animate-spin"
+          className="w-6 h-6 animate-spin"
           viewBox="0 0 32 32"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            clip-rule="evenodd"
+            clipRule="evenodd"
             d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
             fill="currentColor"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
         </svg>
 
@@ -21,3 +21,5 @@ export default () => {
     </div>
   );
 };
+
+export default Loader;
--- monolit/src/Components/ProductLists.jsx
+++ monolit/src/Components/ProductLists.jsx
@@ -1,42 +1,57 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
-import ProudctCard from "./ProudctCard";
+import { useEffect, useState } from "react";
+import ProudctCard from "./ProuductCard";
 import Seach from "./Seach";
 import Loader from "./Loader";
 
-export default ({loading,setLoading}) => {
-    const [ products , setProducts ] = useState([]);
-    const [query, setQuery ] = useState({
-        category: "",
-        title: ""
-    })
+const ProductList = ({ loading, setLoading }) => {
+  const [products, setProducts] = useState([]);
+  const [query, setQuery] = useState({
+    category: "",
+    title: "",
+  });
 
-    useEffect(() => {
-        if(!products.length){
-            fetechProducts()
-        }
-    }, [products])
-
-   async  function fetechProducts () {
-        setLoading(true);
-        const { data } = await  axios.get("https://fakestoreapi.com/products")
-        setProducts(data);
-        setLoading(false);
+  useEffect(() => {
+    if (!products.length) {
+      fetchProducts();
     }
+  }, [products]);
 
-    function handleChange  (e) {
-        setQuery({...query, [e.target.name] : e.target.value })
+  async function fetchProducts() {
+    setLoading(true);
+    try {
+      const { data } = await axios.get("https://fakestoreapi.com/products");
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    function handleSubmit() {
+  const handleChange = (e) => {
+    setQuery({ ...query, [e.target.name]: e.target.value });
+  };
 
-    }
+//   const handleSubmit = () => {
+//     // Handle form submission based on your requirements
+//     // For example, you can filter products based on the query and update the state
+//     const filteredProducts = /* Your filtering logic here */;
+//     setProducts(filteredProducts);
+//   };
 
-    
-    return  loading ? <Loader /> :<>
-    <Seach query={query}  handleChange={handleChange} handleSubmit={handleSubmit}/>
-<section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-  {products.map((product, index) => <ProudctCard index={index} product={product}/>)}
-</section>
-</>
-}
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Seach query={query} handleChange={handleChange} />
+      <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+        {products.map((product, index) => (
+          <ProudctCard key={index} product={product} />
+        ))}
+      </section>
+    </>
+  );
+};
+
+export default ProductList;
